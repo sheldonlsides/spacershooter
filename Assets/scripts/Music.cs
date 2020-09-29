bca3d5fb6d4e2cc175e344e9d13c314b4dea37dd
Sheldon Sides <sheldon.sides@live.com>
2020-09-28 21:04:19 -0500

    added customzied terrain and skybox

---
+++ Assets/scripts/Music.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Music : MonoBehaviour
+{
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Invoke("loadNextLevel", 3f);
+    }
+
+    private void loadNextLevel()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
---
+++ Assets/scripts/Ship.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Ship : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
