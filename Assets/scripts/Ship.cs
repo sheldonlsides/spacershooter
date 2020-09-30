fa72705eef99b09b758ba0ff19a30f9cc2e5cb45
Sheldon Sides <sheldon.sides@live.com>
2020-09-30 00:17:05 -0500

    finished animation timeline

--- Assets/scripts/Ship.cs
+++ Assets/scripts/Ship.cs
@@ -1,12 +1,51 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.InputSystem;
-
+﻿using UnityEngine;
 public class Ship : MonoBehaviour
 {
+    private ShipController shipController;
+
+    [SerializeField] private  float speed = 4f;
+    [SerializeField] private float xRange = 5f;
+
+    private void Awake()
+    {
+        shipController = new ShipController();
+    }
+
+    private void OnEnable()
+    {
+        shipController.Enable();
+    }
+
+    private void OnDisable()
+    {
+        shipController.Disable();
+    }
+
     // Update is called once per frame
     void Update()
     {
+        float leftRightInput = shipController.Move.Roll.ReadValue<float>();
+        float upDownnInput = shipController.Move.Pitch.ReadValue<float>();
+
+        print($"Moving {leftRightInput}");
+
+        Vector3 currentPosition = transform.position;
+
+        float xThrow = shipController.Move.Roll.ReadValue<float>();
+        float yThrow = shipController.Move.Pitch.ReadValue<float>();
+
+        float xOffSet = xThrow * speed * Time.deltaTime;
+        float yOffSet = yThrow * speed * Time.deltaTime;
+
+        float rawXPos = transform.localPosition.x + xOffSet;
+        float clampedXPos = Mathf.Clamp(rawXPos, -5f, 5f);
+
+        float rawYPos = transform.localPosition.y + yOffSet;
+        float clampedYPos = Mathf.Clamp(rawYPos, -4f, 4f);
+
+        //currentPosition.y += clampedYPos;
+
+        transform.localPosition = new Vector3(clampedXPos, clampedYPos, transform.localPosition.z); ;
+    
     }
 }
---
+++ Assets/scripts/shipController.cs
@@ -0,0 +1,271 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/scripts/shipController.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @ShipController : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @ShipController()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""shipController"",
+    ""maps"": [
+        {
+            ""name"": ""Move"",
+            ""id"": ""aba2da9d-3797-4d4f-bc14-95ebdd5808af"",
+            ""actions"": [
+                {
+                    ""name"": ""Roll"",
+                    ""type"": ""Button"",
+                    ""id"": ""f6085c18-ddba-476c-84ad-d9841dd1b57e"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": ""Press""
+                },
+                {
+                    ""name"": ""Pitch"",
+                    ""type"": ""Button"",
+                    ""id"": ""22ba9abd-0d32-4d15-871d-99f51b5b068f"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": ""Press""
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": ""rollKeyboard"",
+                    ""id"": ""cae0620b-6489-4bc8-94c9-27124bbb592a"",
+                    ""path"": ""1DAxis"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Roll"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""negative"",
+                    ""id"": ""9e94426d-22ed-481b-b36f-3db314fda43b"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Roll"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""positive"",
+                    ""id"": ""c4f49d58-8823-4d1d-876f-155fc73d7ae0"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Roll"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""1D Axis"",
+                    ""id"": ""be036256-30f9-49e4-a194-b0405e3abd11"",
+                    ""path"": ""1DAxis"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Roll"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""negative"",
+                    ""id"": ""274fd297-95e7-49ee-8fc3-d2683e0c6160"",
+                    ""path"": ""<SwitchProControllerHID>/leftStick/left"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Roll"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""positive"",
+                    ""id"": ""10f9d202-d292-4f8f-8169-5a1fddc7e8f6"",
+                    ""path"": ""<Gamepad>/leftStick/right"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Roll"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""pitchKeyboard"",
+                    ""id"": ""d82b71d9-7c39-4a49-8fc6-ed17df7e0f90"",
+                    ""path"": ""1DAxis"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Pitch"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""negative"",
+                    ""id"": ""6b5765e5-b507-4eed-a292-c982aa740ca6"",
+                    ""path"": ""<Keyboard>/w"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Pitch"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""positive"",
+                    ""id"": ""3173dad8-8bce-477a-9df4-245889eeab84"",
+                    ""path"": ""<Keyboard>/s"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Pitch"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""1D Axis"",
+                    ""id"": ""bfce5a93-8088-4e39-9a6b-aca96730f8da"",
+                    ""path"": ""1DAxis"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Pitch"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""negative"",
+                    ""id"": ""ae64d2e4-af61-464a-988b-b680511f2362"",
+                    ""path"": ""<Gamepad>/rightStick/up"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Pitch"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""positive"",
+                    ""id"": ""d480d65c-3324-4b8b-a993-bfed223934f5"",
+                    ""path"": ""<SwitchProControllerHID>/rightStick/down"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Pitch"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // Move
+        m_Move = asset.FindActionMap("Move", throwIfNotFound: true);
+        m_Move_Roll = m_Move.FindAction("Roll", throwIfNotFound: true);
+        m_Move_Pitch = m_Move.FindAction("Pitch", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Move
+    private readonly InputActionMap m_Move;
+    private IMoveActions m_MoveActionsCallbackInterface;
+    private readonly InputAction m_Move_Roll;
+    private readonly InputAction m_Move_Pitch;
+    public struct MoveActions
+    {
+        private @ShipController m_Wrapper;
+        public MoveActions(@ShipController wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Roll => m_Wrapper.m_Move_Roll;
+        public InputAction @Pitch => m_Wrapper.m_Move_Pitch;
+        public InputActionMap Get() { return m_Wrapper.m_Move; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(MoveActions set) { return set.Get(); }
+        public void SetCallbacks(IMoveActions instance)
+        {
+            if (m_Wrapper.m_MoveActionsCallbackInterface != null)
+            {
+                @Roll.started -= m_Wrapper.m_MoveActionsCallbackInterface.OnRoll;
+                @Roll.performed -= m_Wrapper.m_MoveActionsCallbackInterface.OnRoll;
+                @Roll.canceled -= m_Wrapper.m_MoveActionsCallbackInterface.OnRoll;
+                @Pitch.started -= m_Wrapper.m_MoveActionsCallbackInterface.OnPitch;
+                @Pitch.performed -= m_Wrapper.m_MoveActionsCallbackInterface.OnPitch;
+                @Pitch.canceled -= m_Wrapper.m_MoveActionsCallbackInterface.OnPitch;
+            }
+            m_Wrapper.m_MoveActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Roll.started += instance.OnRoll;
+                @Roll.performed += instance.OnRoll;
+                @Roll.canceled += instance.OnRoll;
+                @Pitch.started += instance.OnPitch;
+                @Pitch.performed += instance.OnPitch;
+                @Pitch.canceled += instance.OnPitch;
+            }
+        }
+    }
+    public MoveActions @Move => new MoveActions(this);
+    public interface IMoveActions
+    {
+        void OnRoll(InputAction.CallbackContext context);
+        void OnPitch(InputAction.CallbackContext context);
+    }
+}
