76ef33c17261e0e520b2285e014b2ec28d47f63b
Sheldon Sides <sheldon.sides@live.com>
2020-10-04 13:59:09 -0500

    added dynamic box collider to enemy gameobjects and added explosion effect to enemies

---
+++ Assets/scripts/Enemy.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] GameObject deathFX = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        AddNonTriggerBoxCollider();
+    }
+
+    //adds box collider dynamically to gameobject
+    private void AddNonTriggerBoxCollider()
+    {
+        Collider boxCollider = gameObject.AddComponent<BoxCollider>();
+        boxCollider.isTrigger = false;
+
+        deathFX.SetActive(true);
+    }
+
+    //detects whether enemy has been killed
+    private void OnParticleCollision(GameObject other)
+    {
+        print($"Enemy hit {gameObject.name}");
+        Instantiate(deathFX, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
